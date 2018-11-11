90e794bfa752ccc91fd3c7e6fc87d505b2a6fa7c
Adrian Novegil <adrian.novegil@gmail.com>
2018-11-12 00:36:52 +0100

    Lambda examples

---
+++ 06-func-orden-sup/03-lambdas/lambdas.hs
@@ -0,0 +1,17 @@
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n =  n:chain (n `div` 2)
+    | odd n  =  n:chain (n*3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+addThree :: (Num a) => a -> a -> a -> a
+addThree x y z = x + y + z
+
+addThree' :: (Num a) => a -> a -> a -> a
+addThree' = \x -> \y -> \z -> x + y + z
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f = \x y -> f y x
